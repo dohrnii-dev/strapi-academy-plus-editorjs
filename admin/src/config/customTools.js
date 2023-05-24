b461fefc409c17b51ae9bd518336892cd260db1e
Walid <walid.r.rashed@gmail.com>
2023-05-24 13:53:11 +0300

    chore: updating glossaries api endpoint

--- admin/src/config/customTools.js
+++ admin/src/config/customTools.js
@@ -63,7 +63,7 @@ const customTools = {
   link: {
     class: LinkAutocomplete,
     config: {
-      endpoint: process.env.AUTO_COMPLETE_ENDPOINT || "/api/glossaries",
+      endpoint: process.env.AUTO_COMPLETE_ENDPOINT || "/api/glossaries/lookup",
       queryParam:
         process.env.AUTO_COMPLETE_ENDPOINT_QUERY_PARAM ||
         "filters[name][$containsi]",
